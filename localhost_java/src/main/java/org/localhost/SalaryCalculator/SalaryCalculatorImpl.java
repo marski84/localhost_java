77ac021f84e340323179f46f34e22b2832796d99
marski84 <marski705@gmail.com>
2024-05-11 15:30:49 +0200

    salary calculator final

--- localhost_java/src/main/java/org/localhost/SalaryCalculator/SalaryCalculatorImpl.java
+++ localhost_java/src/main/java/org/localhost/SalaryCalculator/SalaryCalculatorImpl.java
@@ -3,11 +3,20 @@ package org.localhost.SalaryCalculator;
 import org.localhost.SalaryCalculator.model.SalaryCalculator;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public class SalaryCalculatorImpl {
     public static void main(String[] args) {
         SalaryCalculator salaryCalculator = new SalaryCalculator();
         BigDecimal salary = new BigDecimal( "5000.00");
-        salaryCalculator.calculateSalary(salary);
+        Map<String, Double> taxes= Map.of(
+                "incomeTax", 0.12,
+                "healthInsuranceContribution", 0.09,
+                "pensionInsuracne", 0.0976,
+                "disabilityInsurance", 0.015,
+                "sicknessInsurance", 0.5);
+//        salaryCalculator.calculateSalary(salary);
+//        salaryCalculator.calculateSalary(salary, taxes);
+        System.out.println(salaryCalculator.calculateSalary(salary, taxes));
     }
 }
--- localhost_java/src/main/java/org/localhost/SalaryCalculator/model/SalaryCalculator.java
+++ localhost_java/src/main/java/org/localhost/SalaryCalculator/model/SalaryCalculator.java
@@ -9,9 +9,7 @@ public class SalaryCalculator implements Calculator{
     private Locale defaultLocale = Locale.getDefault();
     private final String PL_ID = "pl_PL";
     private final String GER_ID = "ger_Ger";
-    private final BigDecimal INIT_NET_SALARY = BigDecimal.valueOf(0);
-
-    private BigDecimal salary;
+    private final BigDecimal INIT_TAX = BigDecimal.valueOf(0);
     private Map<String, Double> taxes;
 
     public SalaryCalculator() {
@@ -21,8 +19,7 @@ public class SalaryCalculator implements Calculator{
 
     @Override
     public BigDecimal calculateSalary(BigDecimal salary) {
-        BigDecimal totalTax = INIT_NET_SALARY;
-        this.salary = salary;
+        BigDecimal totalTax = INIT_TAX;
 
         if (String.valueOf(defaultLocale).equals(PL_ID)) {
             this.taxes = Taxes.PL_TAXES;
@@ -33,32 +30,34 @@ public class SalaryCalculator implements Calculator{
         }
 
         for (double taxValue: taxes.values()) {
-            System.out.println(taxValue);
-            BigDecimal tax = this.calculateTax(taxValue);
+            BigDecimal tax = this.calculateTax(taxValue, salary);
             totalTax = totalTax.add(tax);
         }
-        System.out.println("wynik " + totalTax);
-        System.out.println( salary.subtract(totalTax).setScale(2, RoundingMode.HALF_UP));
-        return salary.subtract(totalTax);
+        BigDecimal formatedSalary = salary.subtract(totalTax).setScale(2, RoundingMode.HALF_UP);
+        return formatedSalary;
     }
 
     @Override
     public BigDecimal calculateSalary(BigDecimal salary, Map<String, Double> taxes) {
-        return null;
+        BigDecimal totalTax = INIT_TAX;
+
+        for (double taxValue: taxes.values()) {
+            BigDecimal tax = this.calculateTax(taxValue, salary);
+            totalTax = totalTax.add(tax);
+        }
+
+        BigDecimal formatedSalary = salary.subtract(totalTax).setScale(2, RoundingMode.HALF_UP);
+        return formatedSalary;
     }
 
-    private BigDecimal calculateTax(double taxRate) {
-        System.out.println(this.salary.multiply(new BigDecimal(taxRate)));
-        return this.salary.multiply(new BigDecimal(taxRate));
+    private BigDecimal calculateTax(double taxRate, BigDecimal salary) {
+        return salary.multiply(new BigDecimal(taxRate));
     }
 
     public Locale getDefaultLocale() {
         return defaultLocale;
     }
 
-    public BigDecimal getSalary() {
-        return salary;
-    }
 
     public Map<String, Double> getTaxes() {
         return taxes;
