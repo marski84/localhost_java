b93f7deddb3a9375a80b8044662009eff72ef89f
marski84 <marski705@gmail.com>
2024-05-12 16:19:58 +0200

    words finder final

--- localhost_java/src/main/java/org/localhost/Words_finder/logic/WordsFinder.java
+++ localhost_java/src/main/java/org/localhost/Words_finder/logic/WordsFinder.java
@@ -1,49 +1,31 @@
 package org.localhost.Words_finder.logic;
-
-//Map<String, Integer> wordsFinder(String text)
-
 import java.util.*;
-import java.util.stream.Stream;
 
 public class WordsFinder {
-    private static final int INIT_COUNTER = 0;
-    private int amountOfOccurencesCounter = INIT_COUNTER;
-
 
     public Map<String, Integer> sortAndCountWords(String inputText) {
-        Map<String, Integer> wordsMap = new HashMap();
+        Map<String, Integer> wordsMap = new LinkedHashMap<>();
 
-        String[] inputArray = inputText.split(" ");
+        String cleanText = inputText.replaceAll("[\\p{Punct}&&[^']]", "");
+        String[] inputArray = cleanText.split(" ");
 
         List<String> sortedStringList = new ArrayList<String>(Arrays.asList(inputArray))
-                .stream().sorted()
+                .stream()
+                .sorted()
                 .toList();
 
         for (String word : sortedStringList) {
-            if (!wordsMap.containsKey(word)) {
-                wordsMap.put(word, 1);
-            } else {
-                wordsMap.put(word, wordsMap.get(word)+ 1);
-            }
+            handleAddToMap(word, wordsMap);
         }
         System.out.println(wordsMap);
-
-
-
-
-//        System.out.println(sortedStringList);
-//        wordsMap.put("okok", 1);
-//        if (wordsMap.containsKey("okok")){
-//            Integer word = wordsMap.get("okok");
-//            word++;
-//            System.out.println("jest");
-//            System.out.println(word);
-//            wordsMap.put("okok", word++);
-//        }
-//        System.out.println(wordsMap);
-
-
-
         return wordsMap;
+    }
+
+    private void handleAddToMap(String word, Map<String, Integer> wordsMap) {
+        if (!wordsMap.containsKey(word)) {
+            wordsMap.put(word.toLowerCase(), 1);
+        } else {
+            wordsMap.put(word, wordsMap.get(word)+ 1);
+        }
     }
 }
